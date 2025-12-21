a04dd1a63937063b5ed38498babedfe2993a8749
Aldo Saputra / Informatika <aldopadaungan@gmail.com>
2025-12-21 17:49:46 +0800

    OOP02

---
+++ Membuat method pada class yang berisi field.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+
+class OOP2 {
+    int panjang;
+    int lebar;
+    int hasil;
+    void luas(){
+        hasil = panjang * lebar;
+        System.out.println(hasil);
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        OOP2 luasperpanjang = new OOP2();
+        luasperpanjang.panjang = input.nextInt();
+        luasperpanjang.lebar = input.nextInt();
+        luasperpanjang.luas();
+        
+    }
+
+}
