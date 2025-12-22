17e9476a90ab313ec5c82281a881e0c5b6269bea
Aldo Saputra / Informatika <aldopadaungan@gmail.com>
2025-12-22 09:42:52 +0800

    OOP01

--- Membuatclassdanobject.java
+++ Membuatclassdanobject.java
@@ -1,11 +1,11 @@
 
-class ClassAndObject {
+class ClassAndObject { //class
 
     String namapendek;
-    String namapanjang;
+    String namapanjang; // field
 
     public static void main(String[] args) {
-        var ClassAndObject = new ClassAndObject();
+        var ClassAndObject = new ClassAndObject(); // object
         ClassAndObject.namapendek = "Aldo";
         ClassAndObject.namapanjang = "Saputra";
         String c = ClassAndObject.namapanjang;
