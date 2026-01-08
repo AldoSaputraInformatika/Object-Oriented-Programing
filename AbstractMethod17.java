fdd9770adafb07d67a6b32414f262299a706ee40
Aldo Saputra / Informatika <aldopadaungan@gmail.com>
2026-01-08 14:24:27 +0800

    AbstractClass17.java

---
+++ AbstractMethod17.java
@@ -0,0 +1,30 @@
+//Class Utama (abstract)
+
+package AbstractMethod;
+
+public abstract class Animal {
+    String name;
+    abstract  void dog();
+    
+}
+//Subclass override
+package AbstractMethod;
+
+public class Dog extends Animal{
+    void dog(){
+        System.out.println("This dog name is " + name);
+    }
+}
+//Class App
+package AbstractMethod;
+
+public class AnimalApp {
+
+    public static void main(String[] args) {
+
+        Animal animal = new Dog();
+        animal.name = "Blacky";
+        animal.dog();
+    }
+
+}
